02cbc7f79233beb2ec2744bf39221a28b713d3ce
Cosmino <cosminoance@gmail.com>
2018-07-29 13:30:31 +0300

    remove not needed

--- src/main/java/constants/AddedSince.java
+++
@@ -1,8 +0,0 @@
-package constants;
-
-public abstract class AddedSince {
-	public static final String Last24h="1";
-	public static final String Last3Days="3";
-	public static final String Last7Days="7";
-	public static final String Last14Days="14";
-}
